88d06784d1e82aa69ec1e7abc768df29a0f157e8
Jeff Emery <jemery@envisionradiology.com>
2020-12-31 12:46:04 -0700

    Console App - Sample Console Host

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleHostSample
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Entering Main!");
+
+            await CreateHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.Configure<WorkerOptions>(context.Configuration.GetSection(WorkerOptions.Settings));
+                    services.AddHostedService<LoopingHostedService>();
+                    //services.AddHostedService<LoopingBackgroundService>();
+                });
+        }
+    }
+
+    /* 2 ways to start a background service
+     * Deriving from BackgroundService or implementing IHostedService
+     * Either way seems to work, BackgroundService is probably better until/unless
+     * there is some reason driving the necessity of IHostedService
+     */
+
+    public class LoopingBackgroundService : BackgroundService
+    {
+        private readonly ILogger<LoopingBackgroundService> logger;
+        private readonly WorkerOptions options;
+
+        public LoopingBackgroundService(ILogger<LoopingBackgroundService> logger, IOptions<WorkerOptions> options)
+        {
+            this.logger = logger;
+            this.options = options.Value;
+        }
+        
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.Register(() => logger.LogDebug("Worker loop received cancel"));
+
+            logger.LogInformation("Entering worker loop");
+
+            Task.Run(async () =>
+            {
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    await Task.Delay(options.Delay, stoppingToken);
+                    logger.LogInformation($"Doing work... waiting {options.Delay}");
+                }
+            }, stoppingToken).ConfigureAwait(false);
+
+            logger.LogInformation("Exiting worker loop");
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public class LoopingHostedService : IHostedService
+    {
+        private readonly IConfiguration config;
+        private readonly WorkerOptions options;
+        private readonly ILogger logger;
+        private readonly IHostEnvironment hostEnvironment;
+
+        public LoopingHostedService(IConfiguration config, IOptions<WorkerOptions> options, ILogger<LoopingHostedService> logger, IHostEnvironment hostEnvironment)
+        {
+            this.config = config;
+            this.options = options.Value;
+            this.logger = logger;
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Hello from StartAsync");
+            var loopTask = WorkerLoop(cancellationToken);
+            logger.LogInformation("Could do more things while loop runs...");
+            return loopTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Goodbye from StopAsync");
+            return Task.CompletedTask;
+        }
+
+        private Task WorkerLoop(CancellationToken cancellationToken)
+        {
+            cancellationToken.Register(() => logger.LogDebug("Worker loop received cancel"));
+
+            logger.LogInformation("Entering worker loop");
+
+            Task.Run(async () =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(options.Delay);
+                    logger.LogInformation($"Doing work... waiting {options.Delay}");
+                }
+            }, cancellationToken).ConfigureAwait(false);
+
+            logger.LogInformation("Exiting worker loop");
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public class WorkerOptions
+    {
+        public const string Settings = "WorkerOptions";
+        public string Message { get; set; }
+        public int Delay { get; set; }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,65 @@
+﻿## Sample Console Host
+
+This project demonstrates a generic host .NET Core from a Console App Visual Studio 2019 template.
+
+The majority of the configuration, boilerplate and default setup comes from [Microsoft - .NET Generic Host in ASP.NET Core](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-5.0)
+
+##### Packages
+
+Configuration is provided with [Microsoft.Extensions.Configuration](https://www.nuget.org/packages/Microsoft.Extensions.Configuration/)
+
+The generic host is provided with [Microsoft.Extensions.Hosting](https://www.nuget.org/packages/Microsoft.Extensions.Hosting/)
+
+#### Sample Project
+
+This sample provides a demonstration of the default services provided by [`CreateDefaultBuilder`](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-5.0#default-builder-settings). The default builder sets up these services by default so there is very little boilerplate code. 
+
+##### [Default Builder](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/host/generic-host?view=aspnetcore-5.0#default-builder-settings)
+
+The initialization boils down to the following method. The configuration also demonstrates using the configuration `IOptions` pattern and adding a derived hosted service `BackgroundService : IHostedService1`.
+
+```csharp
+Host.CreateDefaultBuilder(args).ConfigureServices((context, services) =>
+{
+    services.Configure<WorkerOptions>(context.Configuration.GetSection(WorkerOptions.Settings));
+    services.AddHostedService<LoopingHostedService>();
+});
+```
+
+##### [Configuration](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-5.0)
+
+Configuration is automatically loaded from `appsettings.json` and `appsettings.{Environment}.json`
+
+##### [Dependency Injection](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/dependency-injection?view=aspnetcore-5.0)
+
+Dependency injection is provided automatically, with service registration being done in `ConfigureServices(IServiceCollection services)`.
+
+```csharp
+ConfigureServices((context, services) =>
+{
+    services.Configure<WorkerOptions>(context.Configuration.GetSection(WorkerOptions.Settings));
+    services.AddHostedService<LoopingHostedService>();
+});
+```
+
+##### [Binding `IOptions`](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/options?view=aspnetcore-5.0)
+
+And example of binding settings from `appsettings.json` to a typed object that uses `IOptions<T>` to inject settings into classes is demonstrated.
+
+##### [Environment Variables](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/environments?view=aspnetcore-5.0)
+
+Environment variables are provided by default with prefix `DOTNET_`. Variables are set explicitly in `launchSettings.json` or via project debug properties. Once set,  `IHostEnvironment` is available through dependency injection.
+
+##### [Logging](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-5.0)
+
+Logging is automatically setup, providing the `ILogger<>` interface with dependency injection. Logger configuration is automatically read from `appsettings.json`
+
+##### [`BackgroundService` and `IHostedService`](https://docs.microsoft.com/en-us/dotnet/architecture/microservices/multi-container-microservice-net-applications/background-tasks-with-ihostedservice)
+
+There are two implementations of a background service. Either way works; however, `BackgroundService` is probably better until or unless there is some reason driving the necessity of IHostedService
+
+There is quite a bit to [know and understand about `Task` and `async/await`](https://www.pluralsight.com/guides/using-task-run-async-await). Some of it boils down to IO bound work should be awaited while CPU bound work should be queued with `Task.Run`.
+
+##### [Cancellation Tokens](https://docs.microsoft.com/en-us/dotnet/standard/threading/cancellation-in-managed-threads)
+
+Cancellation tokens are used with `BackgroundService` to signal the shutdown of the host. They could also be used to signal a stop to the service without shutting down the host.
